0bb90ee9c719044310fb252e7cc930cdfdd2b028
root <root@LAP-JCHOI.QCIConsulting.com>
2023-11-06 10:55:20 -0600

    ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,140 @@
-import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let appComponent: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [FormsModule, ReactiveFormsModule],
+    }).compileComponents();
   });
 
-  it(`should have as title 'form'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('form');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    appComponent = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('form app is running!');
+  });
+
+  it('should create the AppComponent', () => {
+    expect(appComponent).toBeTruthy();
+  });
+
+  it('should have title form', () => {
+    expect(appComponent.title).toEqual('form');
+  });
+
+  describe('onSubmit', () => {
+    it('should log', () => {
+      const componentSpy = jest.spyOn(appComponent, 'submitFeedback');
+      const consoleSpy = jest.spyOn(console, 'log');
+      appComponent.submitFeedback();
+      expect(componentSpy).toHaveBeenCalledTimes(1);
+      expect(consoleSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should receive correct parameters', () => {
+      const consoleSpy = jest.spyOn(console, 'log');
+      appComponent.submitFeedback();
+      expect(consoleSpy).toHaveBeenCalledWith(
+        'Feedback form [Submit] - ',
+        appComponent.feedbackForm.value
+      );
+      console.log(appComponent.feedbackForm.value);
+    });
+  });
+
+  describe('form', () => {
+    describe('email', () => {
+      it('should pass validate email', () => {
+        appComponent.feedbackForm.setValue({
+          emailAddress: 'jchoi@qci.com',
+          phoneNumber: '515-864-9309',
+          feedback: 'abcdef',
+          feedbackTitle: 'example',
+          favoriteAnimal: 'cat',
+        });
+
+        expect(appComponent.feedbackForm.valid).toEqual(true);
+      });
+
+      it('should fail invalid email', () => {
+        appComponent.feedbackForm.setValue({
+          emailAddress: 'jchoiqci.com',
+          phoneNumber: '515-864-9309',
+          feedbackTitle: 'example title',
+          feedback: 'abcdef',
+          favoriteAnimal: 'cat',
+        });
+
+        expect(appComponent.feedbackForm.valid).toEqual(false);
+      });
+
+      it('should require email field', () => {
+        expect(() =>
+          appComponent.feedbackForm.setValue({
+            phoneNumber: '515-864-9309',
+            feedbackTitle: 'example title',
+            feedback: 'abcdef',
+            favoriteAnimal: 'cat',
+          })
+        ).toThrowError();
+      });
+    });
+
+    describe('phoneNumber', () => {
+      it('should require phoneNumber', () => {
+        expect(() =>
+          appComponent.feedbackForm.setValue({
+            emailAddress: 'jchoiqci.com',
+            feedbackTitle: 'example title',
+            feedback: 'abcdef',
+            favoriteAnimal: 'cat',
+          })
+        ).toThrowError();
+      });
+    });
+
+    describe('feedback title', () => {
+      it('should require feedback title', () => {
+        expect(() =>
+          appComponent.feedbackForm.setValue({
+            emailAddress: 'jchoiqci.com',
+            phoneNumber: '515-864-9309',
+            feedback: 'abcdef',
+            favoriteAnimal: 'cat',
+          })
+        ).toThrowError();
+      });
+    });
+
+    describe('feedback', () => {
+      it('should require feedback', () => {
+        expect(() =>
+          appComponent.feedbackForm.setValue({
+            emailAddress: 'jchoiqci.com',
+            phoneNumber: '515-864-9309',
+            feedbackTitle: 'abcdef',
+            favoriteAnimal: 'cat',
+          })
+        ).toThrowError();
+      });
+    });
+
+    describe('favorite animal', () => {
+      it('should require favorite animal', () => {
+        expect(() =>
+          appComponent.feedbackForm.setValue({
+            emailAddress: 'jchoiqci.com',
+            phoneNumber: '515-864-9309',
+            feedbackTitle: 'abcdef',
+            feedback: 'abcdef',
+          })
+        ).toThrowError();
+      });
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form';
+
+  // feedback: Feedback = new Feedback('', '', 2, '', '');
+  feedbackForm!: FormGroup;
+  animals = ['cat', 'dog', 'tiger', 'racon', 'others'];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.feedbackForm = new FormGroup({
+      emailAddress: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+
+      phoneNumber: new FormControl('', [Validators.required]),
+      feedbackTitle: new FormControl('', [Validators.required]),
+      feedback: new FormControl('', [Validators.required]),
+      favoriteAnimal: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submitFeedback() {
+    // console.log('Feedback form [Submit] - ', this.feedback)
+    console.log('Feedback form [Submit] - ', this.feedbackForm.value);
+  }
 }
